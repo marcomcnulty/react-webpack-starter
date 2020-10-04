1662c0dabbc08c6bd757fc52494cc4adae753a3c
marcomcnultycodes <marcomcnultycodes@gmail.com>
2020-10-04 22:41:43 +0100

    👻 INIT: first commit

---
+++ src/app/components/App/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { hot } from "react-hot-loader";
+import "./App.css";
+
+const App = () => {
+  return <div className="app">Hello</div>;
+};
+
+export default App;
